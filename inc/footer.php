cd2a94734f481bc10e3203ab020624eeaca28824
gjsproenca <gjsproenca@gmail.com>
2018-06-26 12:12:14 +0100

    add bootstrap, add template

---
+++ inc/footer.php
@@ -0,0 +1,6 @@
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script type="text/javascript" src="../bootstrap-4-1-1-dist/js/jquery-3.3.1.min.js"></script>
+        <script type="text/javascript" src="../bootstrap-4-1-1-dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link type="text/css" href="../bootstrap-4-1-1-dist/css/bootstrap.min.css" rel="stylesheet">
+        
+        <title>Webware</title>
+    </head>
+    <body>
