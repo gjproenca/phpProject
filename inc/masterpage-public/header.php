7b2a346c5fb82d12ff35139c17e955f03462e0f9
gjsproenca <gjsproenca@gmail.com>
2018-06-28 16:53:44 +0100

    add font font awesome, modify folder structure

--- inc/masterpage-public/header.php
+++ inc/masterpage-public/header.php
@@ -6,7 +6,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <!-- Bootstrap CSS -->
-        <link type="text/css" href="../bootstrap-4-1-1-dist/css/bootstrap.min.css" rel="stylesheet">
+        <link type="text/css" href="./../resources/bootstrap-4-1-1-dist/css/bootstrap.min.css" rel="stylesheet">
+
+        <!--My CSS-->
+        <link type="text/css" href="./../resources/css/font-awesome-4-7-0/css/font-awesome.min.css" rel="stylesheet">
 
         <title>Webware</title>
     </head>
@@ -15,18 +18,18 @@
 
             <nav class="navbar navbar-expand-md bg-primary navbar-dark">
                 <div class="container">
-                    <asp:HyperLink class="navbar-brand" ID="HyperLink1" runat="server" NavigateUrl="~/default.aspx">Webware</asp:HyperLink>
+                    <a class="navbar-brand" href="./../public/index.php">Webware</a>
                     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar2SupportedContent">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse text-center justify-content-end" id="navbar2SupportedContent">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <asp:HyperLink class="nav-link" ID="contacts" runat="server" NavigateUrl="~/frontend/contact.aspx"><i class="fa fa-envelope"></i> Contactos</asp:HyperLink>
+                                <a class="nav-link" href="./../public/contacts.php"><i class="fa fa-envelope"></i> Contactos</a>
                             </li>
                         </ul>
-                        <asp:HyperLink class="btn navbar-btn btn-dark ml-2 text-white" ID="signin" runat="server" NavigateUrl="~/frontend/signin.aspx"><i class="fa fa-sign-in"></i> Entrar</asp:HyperLink>
-                        <asp:HyperLink class="btn navbar-btn btn-dark ml-2 text-white" ID="signup" runat="server" NavigateUrl="~/frontend/signup.aspx"><i class="fa fa-user-plus"></i> Registar</asp:HyperLink>
+                        <a class="btn navbar-btn btn-dark ml-2 text-white" ID="signin" href="~/frontend/signin.aspx"><i class="fa fa-sign-in"></i> Entrar</a>
+                        <a class="btn navbar-btn btn-dark ml-2 text-white" ID="signup" href="~/frontend/signup.aspx"><i class="fa fa-user-plus"></i> Registar</a>
                     </div>
                 </div>
             </nav>
--- public/index.php
+++ public/index.php
@@ -1,5 +1,44 @@
-<?php include './../inc/header.php'; ?>
+<?php include './../inc/masterpage-public/header.php'; ?>
 
-asdsad
+<div class="py-5 text-center" style="background-image: url(./../resources/images/warehouse.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center;">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="display-3 mb-4 text-white">Bem-vindo</h1>
+            </div>
+        </div>
+    </div>
+</div>
 
-<?php include './../inc/footer.php'; ?>
+<div class="py-5">
+    <div class="container">
+        <div class="row mb-5">
+            <div class="col-md-7">
+                <h2 class="text-primary">Mais rápidos que uma bala</h2>
+                <p>
+                    Aceda aos seus ficheiros em qualquer lugar e a qualquer hora
+                    <br />
+                    Os nossos serviços de hospedagem são os mais rápidos do mercado, com velocidades de envio/descarga a chegar a 1GB/s.
+                </p>
+            </div>
+            <div class="col-md-5 align-self-center">
+                <img class="img-fluid d-block w-100 img-thumbnail" src="./../resources/images/bullet.jpg">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-5">
+                <img class="img-fluid d-block mb-4 w-100 img-thumbnail" src="./../resources/images/security.jpg">
+            </div>
+            <div class="col-md-7">
+                <h2 class="text-primary pt-3">Segurança</h2>
+                <p>
+                    Os seus ficheiros estão seguros conosco, toda a nossa informação é encriptada com DES (Data Encryption Standard) triplo.
+                    <br />
+                    DES (Data Encryption Standard) triplo uses três chaves individuais de 56 bits cada. O comprimento total da chave é de 168 bits, o que significa que apenas você terá acesso aos seus ficheiros.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include './../inc/masterpage-public/footer.php'; ?>
