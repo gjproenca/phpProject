b64a7c3f72597c15bfdf7be850fb279a03e4322c
gjsproenca <gjsproenca@gmail.com>
2018-06-29 16:12:07 +0100

    fix include contacts

--- public/contacts.php
+++ public/contacts.php
@@ -1,7 +1,7 @@
 <?php
 
 include './../inc/masterpage-public/header.php';
-include('../inc/connection.php');
+include '../inc/connection.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["inputName"];
@@ -41,7 +41,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
             <!--Map-->
             <div class="col-md-6">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3112.1051384428206!2d-9.149991018320703!3d38.73834949593829!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa6833f2027bb59d4!2sCiteforma+-+Centro+de+Forma%C3%A7%C3%A3o+Profissional+dos+Trabalhadores+de+Escrit%C3%B3rio%2C+Com%C3%A9rcio%2C+Servi%C3%A7os+e+Novas+Tecnologias!5e0!3m2!1sen!2spt!4v1528056320966" 
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3112.1051384428206!2d-9.149991018320703!3d38.73834949593829!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa6833f2027bb59d4!2sCiteforma+-+Centro+de+Forma%C3%A7%C3%A3o+Profissional+dos+Trabalhadores+de+Escrit%C3%B3rio%2C+Com%C3%A9rcio%2C+Servi%C3%A7os+e+Novas+Tecnologias!5e0!3m2!1sen!2spt!4v1528056320966"
                         width="100%" height="100%" frameborder="0" style="border: 0" allowfullscreen></iframe>
             </div>
 
@@ -77,4 +77,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </div>
 </div>
 
-<?php include './../inc/masterpage-public/footer.php'; ?>
+<?php include './../inc/masterpage-public/footer.php';?>
