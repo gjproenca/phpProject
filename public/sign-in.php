dd2e80a1e6b507f247e021f49937c774e74a4f59
Goncalo Proenca <gjsproenca@gmail.com>
2018-07-01 17:11:34 +0100

    add sign-in verification, add general UI improvements, fix passowrd not being correctly hashed into table upon signing up

--- public/sign-in.php
+++ public/sign-in.php
@@ -1,28 +1,92 @@
-<?php include './../inc/masterpage-public/header.php';?>
+<?php
+include './../inc/masterpage-public/header.php';
+include '../inc/connection.php';
 
-<div class="py-5">
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($conn->connect_error) {
+        echo '<div class="alert alert-danger mb-0 rounded-0 text-center" role="alert">
+            Falha na ligação à base de dados, por favor tente mais tarde</div>';
+    } else {
+        // Get post data
+        $postUsername = $_POST['inputUsername'];
+        $postPassword = $_POST['inputPassword'];
+
+        $hashPassword = hash('sha512', $postPassword);
+
+        // Get user data
+        $sql = "SELECT `Username`, `Password`, `Admin`, `Active` FROM `user` WHERE `Username` LIKE '$postUsername'";
+        @$query = $conn->query($sql);
+
+        $result = $query->fetch_assoc();
+
+        $resultUsername = $result['Username'];
+        $resultPassword = $result['Password'];
+        $resultAdmin = $result['Admin'];
+        $resultActive = $result['Active'];
+
+        if (!isset($query)) {
+            echo '<div class="alert alert-danger mb-0 rounded-0 text-center" role="alert">
+                Falha ao obter a lista de utilizadores, por favor tente mais tarde</div>';
+        } else if (!isset($result)) {
+            echo '<div class="alert alert-danger mb-0 rounded-0 text-center" role="alert">
+                Não existe este utilizador</div>';
+        } else if ($hashPassword != $resultPassword) {
+            echo '<div class="alert alert-danger mb-0 rounded-0 text-center" role="alert">
+                Senha errada</div>';
+        } else if ($resultAdmin == 1) {
+            if ($resultActive == 1) {
+                echo 'admin';
+                // TODO: session + redirect
+            } else {
+                echo '<div class="alert alert-danger mb-0 rounded-0 text-center" role="alert">
+                    Conta de administrador não ativa</div>';
+            }
+        } else {
+            if ($resultActive == 1) {
+                echo 'user';
+                // TODO: session + redirect
+            } else {
+                echo '<div class="alert alert-danger mb-0 rounded-0 text-center" role="alert">
+                    Conta de utilizador não ativa</div>';
+            }
+        }
+    }
+}
+?>
+
+<div class="py-5 bg-primary text-white">
     <div class="container">
         <div class="row">
             <div class="col-md-3"></div>
             <div class="col-md-6">
-                <div class="card text-white p-5 bg-primary">
+                <div class="p-5 card text-white bg-primary border-secondary">
+
+                    <!-- Form -->
                     <div class="card-body">
                         <h1 class="mb-4">Entrar</h1>
-                        <div class="form-group">
-                            <label>Nome de utilizador</label>
-                            <input class="form-control" name="inputUsername" placeholder="Mínimo 8 carateres" type="text" pattern="^\w{8,}$" required></input>
-                        </div>
-                        <div class="form-group">
-                            <label>Senha</label>
-                            <input class="form-control" name="inputPassword" placeholder="Mínimo 8 carateres" type="password" pattern="^[^\s].{6,}[^\s]$"
-                                required></input>
-                        </div>
-                        <button class="btn btn-dark text-white" name="signin" onclick="signin_Click">
-                            <i class="fa fa-sign-in"></i> Entrar</button>
-                        <input class="btn btn-dark text-white float-sm-right" name="resetSignin" type="reset" value="Limpar" />
-                        <br />
-                        <br />
-                        <a class="btn btn-dark text-white float-sm-right" name="forgotPassword" href="./forgot-password.php">Recuperar senha</a>
+                        <form method="POST" action="">
+                            <div class="form-group">
+                                <label>Nome de utilizador</label>
+                                <input class="form-control" name="inputUsername" placeholder="Mínimo 8 carateres" type="text" pattern="^\w{8,}$" required>
+                            </div>
+                            <small class="form-text text-muted">
+                                O nome tem de conter no mínimo 3 carateres
+                            </small>
+                            <div class="form-group">
+                                <label>Senha</label>
+                                <input class="form-control" name="inputPassword" placeholder="Mínimo 8 carateres" type="password" pattern="^[^\s].{6,}[^\s]$"
+                                    required>
+                                <small class="form-text text-muted">
+                                    A senha tem de conter no mínimo 8 carateres
+                                </small>
+                            </div>
+                            <button class="btn btn-dark text-white" name="signin" onclick="signin_Click">
+                                <i class="fa fa-sign-in"></i> Entrar</button>
+                            <input class="btn btn-dark text-white float-sm-right" name="resetSignin" type="reset" value="Limpar" />
+                            <br />
+                            <br />
+                            <a class="btn btn-dark text-white float-sm-right" name="forgotPassword" href="TODO:./forgot-password.php">Recuperar senha</a>
+                        </form>
                     </div>
                 </div>
             </div>
--- public/sign-up.php
+++ public/sign-up.php
@@ -19,7 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $postEmail = $_POST["inputEmail"];
     $postCountry = $_POST["dropdownListCountries"];
 
-    $hashPassword = hash('sha512', $password);
+    $hashPassword = hash('sha512', $postPassword);
 
     if ($conn->connect_error) {
         echo '<div class="alert alert-danger mb-0 rounded-0 text-center" role="alert">
@@ -36,6 +36,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $sqlEmail = "SELECT `Email` FROM `user` WHERE `Email` LIKE '$postEmail'";
         @$querySqlEmail = $conn->query($sqlEmail);
 
+        // Check if username exists
+        $sqlUsername = "SELECT `Username` FROM `user` WHERE `Username` LIKE '$postUsername'";
+        @$queryUsername = $conn->query($sqlUsername);
+
         // Regiter query
         $sqlRegisto = "INSERT INTO `user` (`CountryId`,`Name`,`Username`,`Password`,`Email`)
                         VALUES ('$resultCountryId','$postName','$postUsername','$hashPassword','$postEmail')";
@@ -43,10 +47,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if ($postPassword !== $postConfirmPassword) {
             echo '<div class="alert alert-danger mb-0 rounded-0 text-center" role="alert">
                 Os campos senha não coincidem</div>';
+        } else if ($queryUsername === false) {
+            echo '<div class="alert alert-danger mb-0 rounded-0 text-center" role="alert">
+                Falha ao verificar o nome de utilizador, por favor tente mais tarde</div>';
+        } else if ($queryUsername->num_rows == 1) {
+            echo '<div class="alert alert-danger mb-0 rounded-0 text-center" role="alert">
+                Nome de utilizador já existente, se se esqueceu da senha e quiser recuperá-la, clique em \'Recuperar senha\'</div>';
         } else if ($querySqlEmail === false) {
             echo '<div class="alert alert-danger mb-0 rounded-0 text-center" role="alert">
                 Falha ao verificar email, por favor tente mais tarde</div>';
-        } else if ($querySqlEmail->num_rows > 0) {
+        } else if ($querySqlEmail->num_rows == 1) {
             echo '<div class="alert alert-danger mb-0 rounded-0 text-center" role="alert">
                 Email já existente, se se esqueceu da senha e quiser recuperá-la, clique em \'Recuperar senha\'</div>';
         } else if ($conn->query($sqlRegisto) === true) {
@@ -54,6 +64,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             echo '<div class="alert alert-success mb-0 rounded-0 text-center" role="alert">
                 Registo efetuado com sucesso</div>';
             // echo "Error: " . $sql . "<br>" . $conn->error; // to check query error
+
+            // TODO: send welcome email
         } else {
             echo '<div class="alert alert-success mb-0 rounded-0 text-center" role="alert">
                 Falha ao efetuar o registo, por favor tente mais tarde</div>';
@@ -73,7 +85,7 @@ $conn->close();
                 <!-- Form -->
                 <div class="card-body">
                     <h1 class="mb-4">Registar</h1>
-                    <form method="POST" action="" onSubmit="return validate();">
+                    <form method="POST" action="">
                         <div class="form-group">
                             <label>Nome</label>
                             <input class="form-control" id="inputName" name="inputName" placeholder="Mínimo 3 carateres" type="text" pattern="^[A-Za-zÀ-Úà-ú]{2,}[A-Za-zÀ-Úà-ú\s]*[A-Za-zÀ-Úà-ú]$"
