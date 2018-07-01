880195e614a9660764506d48157cf1bb3f865557
Goncalo Proenca <gjsproenca@gmail.com>
2018-07-01 20:17:10 +0100

    fix footer links frontend

--- inc/masterpage-frontend/footer.php
+++ inc/masterpage-frontend/footer.php
@@ -5,15 +5,14 @@
                     <h2 class="mb-4 text-secondary">Webware</h2>
                     <p class="text-white">Aceda aos seus ficheiros a qualquer altura, em qualquer lugar</p>
                 </div>
-                <!-- TODO: link paths -->
                 <div class="mt-4 col-md-4">
                     <h2 class="mb-4 text-secondary">Mapa</h2>
                     <ul class="list-unstyled">
-                        <a href="/frontend/user-area.aspx" class="text-white">Início</a>
+                        <a href="./../frontend/index.php" class="text-white">Ficheiros</a>
                         <br>
-                        <a href="/frontend/user-account.aspx" class="text-white">Conta</a>
+                        <a href="./../frontend/edit-account.php" class="text-white">Conta</a>
                         <br>
-                        <a href="/frontend/user-contact.aspx" class="text-white">Contactos</a>
+                        <a href="./../frontend/contact-us.php" class="text-white">Contactos</a>
                     </ul>
                 </div>
                 <div class="mt-4 col-md-4">
