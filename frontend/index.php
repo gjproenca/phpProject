0ec7003e1d594916c50949761fe95bc71675ace3
gjsproenca <gjsproenca@gmail.com>
2018-07-03 12:44:53 +0100

    clean frontend index code

--- frontend/index.php
+++ frontend/index.php
@@ -4,6 +4,7 @@ include './../inc/connection.php';
 
 $userId = $_SESSION['userId'];
 
+// Upload files to folder and perform INSERT query on database
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_FILES['inputFiles'])) {
         foreach ($_FILES['inputFiles']['name'] as $file => $name) {
@@ -11,20 +12,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
             try {
                 if (move_uploaded_file($_FILES['inputFiles']['tmp_name'][$file], './../uploads/' . $path)) {
-                    //TODO: change sql column name from Path to filename
-                    // TODO: add filename coll
                     $sqlFiles = "INSERT INTO `upload` (`UserId`,`FileName`,`Path`) VALUES ($userId,'$name','$path')";
 
                     $conn->query($sqlFiles);
-
                 }
             } catch (Exception $e) {
-                echo $e;
+                //echo $e;
             }
         }
     }
-
 }
+
+// Set active to 0 in databse
+// TODO: might only need id to set files to deative, check table filling href function
 if (isset($_GET['id'])) {
     // if (unlink('./../uploads/' . $_GET['name'])) {
     $sqlTableFiles = "UPDATE `upload` SET `Active` = 0 WHERE `UploadId` = '" . $_GET['id'] . "'";
@@ -34,6 +34,7 @@ if (isset($_GET['id'])) {
     // }
 }
 
+// Variables to populate table with files
 $sqlTableFiles = "SELECT `UploadId`, `FileName` , `Path` FROM `upload` WHERE `Active` = 1";
 $resultTableFiles = $conn->query($sqlTableFiles);
 
@@ -53,10 +54,12 @@ $conn->close();
         </tr>
     </thead>
     <tbody>
+        <!-- Populate table with files -->
         <?php while ($rowTableFiles = $resultTableFiles->fetch_assoc()): ?>
             <tr>
                 <td><?php echo $rowTableFiles['FileName'] ?></td>
                 <td>
+                    <!-- Sending file name ('name') and upload id ('id')  -->
                     <a href="?name=<?php echo $rowTableFiles['Path']; ?>&id=<?php echo $rowTableFiles['UploadId'] ?>" class="btn btn-danger">Eliminar</a>
                 </td>
             </tr>
