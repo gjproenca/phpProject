401e58298e4f048918e094a6a494aa032fead016
gjsproenca <gjsproenca@gmail.com>
2018-07-04 17:11:21 +0100

    modify frontend/index layout

--- frontend/index.php
+++ frontend/index.php
@@ -7,25 +7,24 @@ $userId = $_SESSION['userId'];
 // Upload files to folder and perform INSERT query on database
 // Max file size 2MB, max files on single post 20
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_FILES['inputFile'])) {
+    if (isset($_FILES['inputFile']) && $_FILES['inputFile']['error'] != UPLOAD_ERR_NO_FILE) {
         $fileName = $_FILES['inputFile']['name'];
         $path = date('dmY-His' . time()) . mt_rand() . '-' . $fileName;
 
-        if (($_FILES['inputFile']['size'] < 2100000) && ($_FILES['inputFile']['error'] == 0)) {
-            try {
-                if (move_uploaded_file($_FILES['inputFile']['tmp_name'], './../uploads/' . $path)) {
-                    $sqlFiles = "INSERT INTO `upload` (`UserId`,`FileName`,`Path`) 
+        if (($_FILES['inputFile']['error'] == UPLOAD_ERR_OK) && ($_FILES['inputFile']['error'] != UPLOAD_ERR_INI_SIZE)) {
+            if (move_uploaded_file($_FILES['inputFile']['tmp_name'], './../uploads/' . $path)) {
+                $sqlFiles = "INSERT INTO `upload` (`UserId`,`FileName`,`Path`)
                                     VALUES ($userId,'$fileName','$path')";
 
-                    $conn->query($sqlFiles);
-                }
-            } catch (Exception $e) {
-                echo $e;
+                $conn->query($sqlFiles);
             }
         } else {
             echo '<div class="alert alert-danger mb-0 rounded-0 text-center" role="alert">
-                Não foi possível carregar o ficheiro, tamanho máximo de carregamento 2MB (versão de teste)</div>';
+            Não foi possível carregar o ficheiro, tamanho máximo de carregamento 2MB (versão de teste)</div>';
         }
+    } else {
+        echo '<div class="alert alert-danger mb-0 rounded-0 text-center" role="alert">
+            Nenhum ficheiro selecionado</div>';
     }
 }
 
@@ -45,33 +44,54 @@ $resultTableFiles = $conn->query($sqlTableFiles);
 $conn->close();
 ?>
 
-<form method="POST" enctype="multipart/form-data">
-    <input type="file" name="inputFile">
-    <input type="submit" value="Upload">
-</form>
+<div class="py-5 bg-primary text-white">
+    <div class="container">
+        <div class="row">
+            <!-- Form -->
+            <div class="col-md-12">
+                <form method="POST" enctype="multipart/form-data">
+                    <input type="file" name="inputFile">
+                    <input type="submit" value="Upload">
+                </form>
+            </div>
 
-<table class="table table-bordered table-striped table-hover">
-    <thead>
-        <tr>
-            <th>Ficheiro</th>
-            <th>Acção</th>
-        </tr>
-    </thead>
-    <tbody>
-        <!-- Populate table with files -->
-        <?php while ($rowTableFiles = $resultTableFiles->fetch_assoc()): ?>
-            <tr>
-                <td width="70%">
-                    <?php echo $rowTableFiles['FileName'] ?>
-                </td>
-                <td>
-                    <!-- Sending file name ('name') and upload id ('id')  -->
-                    <a href="<?php echo './../uploads/' . $rowTableFiles['Path'] ?>" class="btn btn-success" download> Descarregar</a>
-                    <a href="?uploadId=<?php echo $rowTableFiles['UploadId'] ?>" class="btn btn-danger">Eliminar</a>
-                </td>
-            </tr>
-        <?php endwhile;?>
-    </tbody>
-</table>
+            <div class="col-md-12 mb-5">
+            </div>
+
+            <div class="col-md-12">
+                <div class="bg-primary border-secondary">
+
+                    <!-- Table -->
+                    <table class="table table-hover table-dark">
+                        <thead class="thead-light">
+                            <tr>
+                                <!-- TODO: see if text-center in file looks better or not -->
+                                <th class="text-center">Ficheiro</th>
+                                <th class="text-center" colspan="2">Acção</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- Populate table with files -->
+                            <?php while ($rowTableFiles = $resultTableFiles->fetch_assoc()): ?>
+                            <tr>
+                                <td width="70%">
+                                    <?php echo $rowTableFiles['FileName'] ?>
+                                </td>
+                                <td>
+                                    <!-- Sending file name ('name') and upload id ('id')  -->
+                                    <a href="<?php echo './../uploads/' . $rowTableFiles['Path'] ?>" class="btn btn-success" download> Descarregar</a>
+                                </td>
+                                <td>
+                                    <a href="?uploadId=<?php echo $rowTableFiles['UploadId'] ?>" class="btn btn-danger">Eliminar</a>
+                                </td>
+                            </tr>
+                            <?php endwhile;?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php include './../inc/masterpage-frontend/footer.php'?>
