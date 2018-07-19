945f301658eb13e1f1539feedff7044ba69c5583
gjsproenca <gjsproenca@gmail.com>
2018-07-19 16:42:50 +0100

    initial commit

---
+++ public/about.php
@@ -0,0 +1,44 @@
+<?php include './../inc/masterpage-public/header.php';?>
+
+<div class="py-5 text-center" style="background-image: url(./../resources/images/warehouse.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center;">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="display-3 mb-4 text-white">Sobre nós</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="py-5">
+    <div class="container">
+        <div class="row mb-5">
+            <div class="col-md-7">
+                <h2 class="text-primary">História</h2>
+                <p>
+                    Empresa criada em 2013, e em forte expansão desde a sua criação.
+                    <br />
+                    Contamos com uma equipa de mais de 300 pessoas e temos servidores espalhados em mais de 30 países, garantindo que os seus dados estão sempre acessíveis.
+                </p>
+            </div>
+            <div class="col-md-5 align-self-center">
+                <img class="img-fluid d-block w-100 img-thumbnail" src="./../resources/images/bullet.jpg">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-5">
+                <img class="img-fluid d-block mb-4 w-100 img-thumbnail" src="./../resources/images/security.jpg">
+            </div>
+            <div class="col-md-7">
+                <h2 class="text-primary pt-3">A nossa missão</h2>
+                <p>
+                    Na nossa empresa temos a maior preocupação com a segurança dos seus ficheiros, tendo uma equipa de engenheiros dedicada a mantê-los seguros.
+                    <br />
+                    Estamos também em constante expansão a nível de capacidade e infraestrutura para garantir a rapidez e fiabilidade dos nossos serviços.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include './../inc/masterpage-public/footer.php';?>
